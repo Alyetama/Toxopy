551b6b09671edf74a6a707d8995e79b010716bd5
Alyetama <56323389+Alyetama@users.noreply.github.com>
2020-08-04 17:56:12 -0500

    :package: add conda envs and update __init__

---
+++ toxopy/__init__.py
@@ -0,0 +1,7 @@
+"""Object-specific python package to run automated tasks in the Chase Lab."""
+
+from .ffsync import ffsync
+from .ffconcat import ffconcat
+from .lazytrim import lazytrim
+
+from .version import __version__, VERSION
