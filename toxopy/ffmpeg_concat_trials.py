bbe48145b5733dd8e4add00f22636ff9f6885d5e
Alyetama <56323389+Alyetama@users.noreply.github.com>
2020-08-04 16:37:32 -0500

    :poop: Add python files

---
+++ toxopy/ffmpeg_concat_trials.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+# todo: add option for either cat or owner trials
+
+import os
+from subprocess import call
+
+
+def ffmpeg_concat(cats, tvp):
+# cats is a list of strings ["cat--id"]
+# tvp: /path/to/trials/videos/
+
+    for cat in cats:
+
+        vid_list = [
+            tvp + cat + '_T2.mp4', tvp + cat + '_T4.mp4', tvp + cat + '_T6.mp4',
+            tvp + cat + '_T8.mp4', tvp + cat + '_T10.mp4'
+        ]
+
+        cat_txt = cat + '.txt'
+
+        if os.path.exists(cat_txt):
+            os.remove(cat_txt)
+
+        file_vids = open(cat_txt, 'a')
+
+        for video in vid_list:
+            files_list = 'file ' + video + '\n'
+            file_vids.write(files_list)
+        file_vids.close()
+
+        cat_sh = cat + '.sh'
+
+        if os.path.exists(cat_sh):
+            os.remove(cat_sh)
+
+        file_sh = open(cat_sh, 'a')
+        ffmpeg_container = " singularity exec /work/chaselab/malyetama/containers/ffmpeg_3.2-nvidia.sif "
+        ffmpeg_command = "#!/bin/bash \n" + ffmpeg_container + "ffmpeg -f concat -safe 0 -i " + cat + ".txt -c copy -y " + tvp + cat + "_cat.mp4"
+
+        file_sh.write(ffmpeg_command)
+        file_sh.close()
+
+        os.chmod('./' + cat + '.sh', 0o755)
+        call('./' + cat + '.sh', shell=True)
---
+++ toxopy/sync.py
@@ -0,0 +1,55 @@
+import os
+from subprocess import run
+import pandas as pd
+import shlex
+
+
+def sync(sync_csv, videos_dir):
+
+    df = pd.read_csv(sync_csv, sep=",")
+
+    names = df.columns
+    df = pd.DataFrame(df, columns=names)
+
+    camera_1 = df.iloc[0][0]
+    camera_2 = df.iloc[1][0]
+
+    offset_1 = df.iloc[0][1]
+    offset_2 = df.iloc[1][1]
+
+    offset = []
+
+    if offset_1 != 0:
+        offset.append(camera_1)
+        offset.append(offset_1)
+        offset.append(camera_2)
+    elif offset_2 != 0 and offset_2 > 0:
+        offset.append(camera_2)
+        offset.append(offset_2)
+        offset.append(camera_1)
+    elif offset_2 != 0 and offset_2 < 0:
+        offset.append(camera_1)
+        offset.append(offset_2 * -1)
+        offset.append(camera_2)
+
+    ffprobe_command = 'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 ' + videos_dir + '/'
+
+    ffprobe_1 = ffprobe_command + offset[0]
+    ffprobe_2 = ffprobe_command + offset[2]
+
+    duration_values = []
+
+    for i in [ffprobe_1, ffprobe_2]:
+        ffprobe_run = os.popen(i)
+        video_duration = ffprobe_run.read()
+        video_duration = video_duration.strip('\n')
+        duration_values.append(video_duration)
+
+    end_time = min(duration_values)
+
+    ffmpeg_run = shlex.split('ffmpeg -ss ' + str(offset[1]) + ' -i ' +
+                             videos_dir + '/' + str(offset[0]) + ' -t ' +
+                             end_time + ' -y ' + videos_dir + '/synced_' +
+                             str(offset[0]))
+
+    run(ffmpeg_run)
