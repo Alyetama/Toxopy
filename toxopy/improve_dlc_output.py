054c13c2bc186aa7229d546841c3359895d97d63
fcatus <f.catus@pm.me>
2020-08-05 14:23:53 -0500

    :hammer: Improve functions

--- toxopy/improve_dlc_output.py
+++ toxopy/improve_dlc_output.py
@@ -2,14 +2,23 @@ import pandas as pd
 from csv import reader
 
 
-def improve_dlc_output(csv_file):
+def improve_dlc_output(csv_file, type=None):
 
     df = pd.read_csv(csv_file)
 
     df = df.iloc[:, :3]
 
     n = len(df)
-    time = 1020
+
+    if type is None:
+        print("Please specify type!")
+
+    if type is 'owner':
+        time = 1020
+
+    if type is 'cat':
+        time = 600
+
     f = n / time
 
     indx = []
@@ -22,17 +31,38 @@ def improve_dlc_output(csv_file):
 
     trials = []
 
-    for i in indx:
-        if i < 300:
-            trials.append('No treatment')
-        elif 300 < i < 480:
-            trials.append('Saline')
-        elif 480 < i < 660:
-            trials.append('Urine')
-        elif 660 < i < 840:
-            trials.append('Saline')
-        elif 840 < i <= 1020:
-            trials.append('Urine')
+
+    if type is None:
+        print("Please specify type!")
+
+
+    elif type is "owner":
+
+        for i in indx:
+            if i < 300:
+                trials.append('No treatment')
+            elif 300 < i < 480:
+                trials.append('First Saline')
+            elif 480 < i < 660:
+                trials.append('First Urine')
+            elif 660 < i < 840:
+                trials.append('Second Saline')
+            elif 840 < i <= 1020:
+                trials.append('Second Urine')
+
+
+    elif type is "cat":
+            t = 120
+            if i < t:
+                trials.append('Cat alone (1)')
+            elif t < i < t*2:
+                trials.append('Cat alone (2)')
+            elif t*2 < i < t*3:
+                trials.append('Cat alone (3)')
+            elif t*3 < i < t*4:
+                trials.append('Cat alone (4)')
+            elif t*4 < i <= t*4:
+                trials.append('Cat alone (5)')
 
     tl = pd.DataFrame(trials, columns=['trial'])
 
@@ -42,3 +72,4 @@ def improve_dlc_output(csv_file):
                   index=False,
                   sep=',',
                   encoding='utf-8')
+
