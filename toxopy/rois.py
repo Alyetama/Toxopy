650e3d25782b6f645786e7a00679866341a8aab2
Alyetama <56323389+Alyetama@users.noreply.github.com>
2020-08-17 09:20:55 -0500

    :ambulance: Hotfix

--- toxopy/rois.py
+++ toxopy/rois.py
@@ -31,7 +31,7 @@ def rois(cat, improved_file):
     plt.plot(time, velocity)
     plt.xlabel('Time in seconds')
     plt.ylabel('Speed in pixels per second')
-    plt.savefig(cat)
+    plt.savefig(cat + '_velocity.png')
 
     # Calculate and plot rois
 
@@ -99,7 +99,7 @@ def rois(cat, improved_file):
         ax.add_patch(rect)
 
     plt.ylim(-100, 600)
-    plt.savefig(cat)
+    plt.savefig(cat + '_rois.png')
 
     res = time_in_each_roi.get_timeinrois_stats(bp_tracking.T,
                                                 rois,
