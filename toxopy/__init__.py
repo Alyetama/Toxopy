eaacc7448230bb5f7fffa1b8d41f9fc5b74dec99
Alyetama <56323389+Alyetama@users.noreply.github.com>
2020-08-18 13:57:06 -0500

    :bookmark: v0.1.5.3

--- toxopy/__init__.py
+++ toxopy/__init__.py
@@ -8,5 +8,6 @@ from .find_distance import find_distance
 from .improve_dlc_csv import improve_dlc_csv
 from .csv2h5 import csv2h5
 from .rois import rois
+from .concat_rois import concat_rois
 
 from .version import __version__, VERSION
