8fc90ab5945764f84691b03e785a4cde594a0454
fcatus <f.catus@pm.me>
2020-08-20 12:37:00 -0500

    :package: Add function to calculate Mann-Whiteny U

--- toxopy/concat_rois.py
+++ toxopy/concat_rois.py
@@ -4,24 +4,45 @@ import glob
 from pathlib import Path
 
 
-def concat_rois(directory):
+def concat_rois(directory, output_dir, trial_type):
+
+    if output_dir.endswith('/') == False:
+
+        raise ValueError(
+            'Output directory does not end with a trailing slash "/"!')
+
+    else:
+        pass
 
     files = glob.glob(directory + '/*.csv')
 
     for file in files:
 
         cat = Path(file).stem
-        cat = cat[:-5]
+ 
+        if trial_type == 'with_owner':
+
+            cat = cat[:-16]
+
+        elif trial_type == 'cat_alone':
+
+            cat = cat[:-15]
 
         df = pd.read_csv(file)
 
         df["cat"] = cat
 
-        df = df[['cat', 'ROI_name', 'transitions_per_roi', 'cumulative_time_in_roi_sec', 'avg_time_in_roi_sec', 'avg_vel_in_roi']]
+        df = df[['cat', 'ROI_name', 'trial', 'transitions_per_roi', 'cumulative_time_in_roi_sec', 'avg_time_in_roi_sec', 'avg_vel_in_roi']]
 
         df.to_csv(file, index=False)
 
 
     combined_csv = pd.concat([pd.read_csv(f) for f in files ])
 
-    combined_csv.to_csv("combined_rois.csv", index=False, encoding='utf-8-sig')
+    if trial_type == 'with_owner':
+
+        combined_csv.to_csv(output_dir + "with_owner_combined_rois.csv", index=False, encoding='utf-8-sig')
+
+    elif trial_type == 'cat_alone':
+
+        combined_csv.to_csv(output_dir + "cat_alone_combined_rois.csv", index=False, encoding='utf-8-sig')
