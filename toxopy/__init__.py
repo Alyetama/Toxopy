454bd72817d632466469fe95dc30d003a24321a3
Alyetama <56323389+Alyetama@users.noreply.github.com>
2020-08-25 15:44:32 -0500

    :package: Add new functions

--- toxopy/__init__.py
+++ toxopy/__init__.py
@@ -10,5 +10,7 @@ from .csv2h5 import csv2h5
 from .analyze_rois import analyze_rois
 from .concat_rois import concat_rois
 from .roi_calc_mw import roi_calc_mw
+from .json2tidycsv import json2tidycsv
+from .obtain_grand_m import obtain_grand_m
 
 from .version import __version__, VERSION
