0548f310cc557d252e3bda86c425fa78dc576fb5
fcatus <f.catus@pm.me>
2020-08-26 02:06:02 -0500

    Add csv concat fun & rebase CI

---
+++ toxopy/concat_csv.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import glob
+
+
+def concat_csv(directory, output_file_name):
+
+
+    files = glob.glob(directory + '/*.csv')
+
+    for file in files:
+
+        df = pd.read_csv(file)
+
+    combined_csv = pd.concat([pd.read_csv(f) for f in files])
+
+    combined_csv.to_csv(directory + '/' + output_file_name + '.csv',
+                            index=False,
+                            encoding='utf-8-sig')
