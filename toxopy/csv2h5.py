10f2694c90ef41bc369e3510c501bb766b47d185
Alyetama <56323389+Alyetama@users.noreply.github.com>
2020-08-30 17:05:09 -0500

    :hammer: Improve code quality

--- toxopy/csv2h5.py
+++ toxopy/csv2h5.py
@@ -1,34 +1,33 @@
 import pandas as pd
 from tqdm import tqdm
 import glob
+from pathlib import Path
 
 
-def csv2h5(directory=None, files=None):
+def csv2h5(path, head=[1]):
+    
+    def toh5(file):
+        df = pd.read_csv(file, header=head, index_col=[0])
+        
+        return df.to_hdf(Path(file).stem + '.h5',
+          'data',
+          mode='w',
+          format='table')
 
-    if directory is None:
+    
+    if isinstance(path, str):
 
-        for file in tqdm(files):
-
-            df = pd.read_csv(file)
-
-            df.to_hdf(file.strip('.csv') + '.h5',
-                      'data',
-                      mode='w',
-                      format='table')
-            del df
-
-    else:
-
-        csv_files_dir = directory + '/*.csv'
+        csv_files_dir = path + '/*.csv'
 
         files = glob.glob(csv_files_dir)
 
         for file in tqdm(files):
+            
+            toh5(file)
+            
+            
+    elif isinstance(path, list):
+        
+        for file in tqdm(path):
 
-            df = pd.read_csv(file)
-
-            df.to_hdf(file.strip('.csv') + '.h5',
-                      'data',
-                      mode='w',
-                      format='table')
-            del df
+            toh5(file)
