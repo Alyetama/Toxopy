ee96dc70eb3920dadaaf3122c1f0de6e9d56957b
Alyetama <56323389+Alyetama@users.noreply.github.com>
2020-09-05 14:44:51 -0500

    :bookmark: v0.4.0

---
+++ toxopy/correct_times.py
@@ -0,0 +1,63 @@
+import pandas as pd
+from pathlib import Path
+import glob
+from toxopy import trials_cap, trials
+from os import remove
+from tqdm import tqdm
+
+
+def correct_times(csv_dir, output_dir):
+
+	"""'csv_dir' is the path to the folder containing the combined csv files"""
+
+	files = glob.glob(f'{csv_dir}/*.csv')
+
+	tls = trials_cap()
+
+	trials_times = [300, 420, 600, 720, 900, 1020, 1200, 1320, 1500, 1620]
+
+	additions = [0, 300, 120, 480, 240, 660, 360, 840, 480, 1020]
+
+
+	def subdf(variable):
+
+		return df[df['trial'] == trial][variable]
+
+
+	for file in tqdm(files):
+
+		df = pd.read_csv(file, header=[0])
+
+		cat = Path(file).stem
+
+		for trial, ttime, ad in zip(tls, trials_times, additions):
+
+			time = df.loc[(df['trial'] == trial)]['time']
+
+			diff = max(time) - min(time)
+
+			if trial == trial and diff > ttime:
+
+				raise ValueError('Fails!', cat, trial, diff)
+
+			elif trial == trial and diff < ttime:
+
+				t, v, a, r = subdf('time') + ad, subdf('velocity_loess05'), subdf('acceleration_loess05'), subdf('trial')
+
+				fdf = pd.DataFrame([t, v, a, r]).T
+
+				fdf.to_csv(f'{output_dir}/{cat}_{trial}.csv', index=False, encoding='utf-8-sig')
+
+		fs = glob.glob(f'{output_dir}/{cat}_*.csv')
+
+		ccsv = pd.concat([pd.read_csv(i) for i in fs])
+
+		old_names, new_names = trials_cap(), trials()
+
+		ccsv['trial'] = ccsv['trial'].replace(old_names, new_names)
+
+		ccsv = ccsv.sort_values(by=['time'])
+
+		ccsv.to_csv(f'{output_dir}/{cat}.csv', index=False, encoding='utf-8-sig')
+
+		[remove(i) for i in fs]
---
+++ toxopy/one_cat_one_file.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from glob import glob
+from pathlib import Path
+
+
+def one_cat_one_file(dir1, dir2, output_dir):
+
+	"""'dir1' is the dir with cat_alone_improved csv files
+	'dir2' is the dir with with_owner_improved csv files"""
+
+	cat_alone, with_owner = glob(dir1 + '/*.csv'), glob(dir2 + '/*.csv')
+
+	for f1, f2 in zip(sorted(cat_alone), sorted(with_owner)):
+
+		cat1, cat2 = Path(f1).stem[:-10], Path(f2).stem[:-11]
+
+		files = []
+
+		if cat1 == cat2:
+			files.append(f1)
+			files.append(f2)
+
+		else:
+			raise ValueError('Something is wrong!')
+
+		combined_csv = pd.concat([pd.read_csv(f, usecols = ['time', 'velocity_loess05', 'acceleration_loess05', 'trial']) for f in files])
+
+		combined_csv.to_csv(f'{output_dir}/{cat1}.csv', index=False, encoding='utf-8-sig')
