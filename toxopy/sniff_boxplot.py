fddb3ff0203cb613e6a916ee26d2b253f847d937
fcatus <56323389+fcatus@users.noreply.github.com>
2020-09-14 11:21:31 -0500

    Update sniff_boxplot.py

--- toxopy/sniff_boxplot.py
+++ toxopy/sniff_boxplot.py
@@ -24,79 +24,79 @@ def sniff_boxplot(csv_file, colors=False, jitter=False, title=False, save=False,
     df = df0[cols].copy()
     tls = trials()
 
-     Negative, Positive = {}, {}
-      dcts, strs = [Negative, Positive], ['Negative', 'Positive']
+    Negative, Positive = {}, {}
+    dcts, strs = [Negative, Positive], ['Negative', 'Positive']
 
-       for s, r in zip(dcts, strs):
-            for i, j, in zip(tls[2::2], range(2, 6)):
-                s[i] = df[df['infection_status'] == r][cols[j]]
+    for s, r in zip(dcts, strs):
+          for i, j, in zip(tls[2::2], range(2, 6)):
+              s[i] = df[df['infection_status'] == r][cols[j]]
 
-        def itrchain(ls):
-            return list(itertools.chain.from_iterable(ls))
+    def itrchain(ls):
+        return list(itertools.chain.from_iterable(ls))
 
-        def achain(ls, x):
-            return ls.append(list(np.repeat(x, len(i[j]))))
+    def achain(ls, x):
+        return ls.append(list(np.repeat(x, len(i[j]))))
 
-        df2 = pd.DataFrame()
+    df2 = pd.DataFrame()
 
-        s, v, t = [], [], []
+    s, v, t = [], [], []
 
-        for i, z in zip(dcts, strs):
-            for j in tls[2::2]:
-                achain(s, z), v.append(list(i[j])), achain(t, j)
+    for i, z in zip(dcts, strs):
+        for j in tls[2::2]:
+            achain(s, z), v.append(list(i[j])), achain(t, j)
 
-        df2['status'], df2['value'], df2['trial'] = itrchain(
-            s), itrchain(v), itrchain(t)
+    df2['status'], df2['value'], df2['trial'] = itrchain(
+        s), itrchain(v), itrchain(t)
 
-        sns.set(style="ticks", font_scale=1)
+    sns.set(style="ticks", font_scale=1)
 
-        plt.figure(figsize=(13, 5), dpi=100)
+    plt.figure(figsize=(13, 5), dpi=100)
 
-        if colors is False:
-            my_pal = {"Negative": "#00FFFF", "Positive": "#E60E3C"}
+    if colors is False:
+        my_pal = {"Negative": "#00FFFF", "Positive": "#E60E3C"}
+    else:
+        my_pal = {"Negative": colors[0], "Positive": colors[1]}
+
+    ax = sns.boxplot(x='trial',
+                     y='value',
+                     data=df2,
+                     hue='status',
+                     palette=my_pal)
+
+    if jitter is True:
+
+        sns.stripplot(x='trial',
+                      y='value',
+                      data=df2,
+                      color='black',
+                      size=3,
+                      jitter=1)
+
+    '''add vertical lines to seperate boxplots pairs (style)'''
+    for i in range(3):
+        plt.vlines(i + .5,
+                   1,
+                   20,
+                   linestyles='solid',
+                   colors='black',
+                   alpha=0.2)
+
+    ax.set(xlabel='Trial', ylabel='Vial Sniff Duration')
+
+    plt.legend(title='Infection Status')
+
+    if title is not False:
+        plt.title(title, fontsize=14)
+    else:
+        pass
+
+    fig = ax.get_figure()
+
+    if save is True:
+        if output_dir is not False:
+            name = f'{output_dir}/sniff_boxplot.png'
         else:
-            my_pal = {"Negative": colors[0], "Positive": colors[1]}
+            name = 'sniff_boxplot.png'
+        fig.savefig(name, bbox_inches="tight", dpi=100, pad_inches=0.1)
 
-        ax = sns.boxplot(x='trial',
-                         y='value',
-                         data=df2,
-                         hue='status',
-                         palette=my_pal)
-
-        if jitter is True:
-
-            sns.stripplot(x='trial',
-                          y='value',
-                          data=df2,
-                          color='black',
-                          size=3,
-                          jitter=1)
-
-        '''add vertical lines to seperate boxplots pairs (style)'''
-        for i in range(3):
-            plt.vlines(i + .5,
-                       1,
-                       20,
-                       linestyles='solid',
-                       colors='black',
-                       alpha=0.2)
-
-        ax.set(xlabel='Trial', ylabel='Vial Sniff Duration')
-
-        plt.legend(title='Infection Status')
-
-        if title is not False:
-            plt.title(title, fontsize=14)
-        else:
-            pass
-
-        fig = ax.get_figure()
-
-        if save is True:
-            if output_dir is not False:
-                name = f'{output_dir}/sniff_boxplot.png'
-            else:
-                name = 'sniff_boxplot.png'
-            fig.savefig(name, bbox_inches="tight", dpi=100, pad_inches=0.1)
-
-        plt.show()
+    plt.show()
