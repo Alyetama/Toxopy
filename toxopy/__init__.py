8c030e4bdc2072129d5df9a2d1afcb3399f008bd
Alyetama <56323389+Alyetama@users.noreply.github.com>
2020-09-15 16:42:17 -0500

    :Package: Merge Mann-Whitney U tests into one func

--- toxopy/__init__.py
+++ toxopy/__init__.py
@@ -10,14 +10,12 @@ from .concat_rois import concat_rois
 from .roi_calc_mw import roi_calc_mw
 from .json2tidycsv import json2tidycsv
 from .obtain_grand_m import obtain_grand_m
-from .calc_dlc_mw import calc_dlc_mw
 from .dlcboxplot import dlcboxplot
 from .fwarnings import fwarnings
 from .combine_dlc_improved import combine_dlc_improved
 from .analyze_turnpoints import analyze_turnpoints
 from .return_sem import return_sem
 from .sniff_boxplot import sniff_boxplot
-from .time_budget_mw import time_budget_mw
-from .latency_mw import latency_mw
+from .MannWhitney_U import MannWhitney_U
 
 from .version import __version__, VERSION
