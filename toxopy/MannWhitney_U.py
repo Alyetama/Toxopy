a5c5a36ab3f772196de85cde85f326d9ac43b3c8
Alyetama <56323389+Alyetama@users.noreply.github.com>
2020-09-16 16:23:27 -0500

    :hammer: Update MannWhitney_U()

--- toxopy/MannWhitney_U.py
+++ toxopy/MannWhitney_U.py
@@ -1,3 +1,9 @@
+"""
+Toxopy (https://github.com/bchaselab/Toxopy)
+© M. Alyetama, University of Nebraska at Omaha
+Licensed under the terms of the MIT license
+"""
+
 from scipy.stats import mannwhitneyu
 import pandas as pd
 from toxopy import trials, nadlc
@@ -27,7 +33,14 @@ def MannWhitney_U(csv_file, test, drop_non_dlc=True, only_sig=False):
             result = 'reject H0'
         return result
 
+    """
+    << TIME BUDGET >>
+    The time budget of a cat for a trial was determined by summing the times
+    it spent on the individual behaviors assigned to each behavioral category
+    (Affiliative, Calm, Exploration/Locomotion, and Fear). 
+    """
     if test == 'time_budget':
+
         def time_budget_mw(csv_file, only_sig):
 
             df = pd.read_csv(csv_file)
@@ -62,20 +75,28 @@ def MannWhitney_U(csv_file, test, drop_non_dlc=True, only_sig=False):
                 for b in behaviors:
                     mw_res = calc_mw(t, b)
                     res = f'{b} ==> {mw_res}'
-
                     if mw_res is not None:
                         print(res)
 
         time_budget_mw(csv_file, only_sig)
 
+    """
+    << The Latency Test >>
+    The latency of the cat to exit the carrier, the latency test, was defined
+    as the time from when the owner opened the carrier to the time when the
+    cat’s entire body was outside of the carrier. Scores on a single behavior
+    “latency to exit the carrier” were used to compute results for
+    the latency test. 
+    """
+
     elif test == 'latency':
+
         def latency_mw(csv_file):
 
             def slct(status):
                 return list(df.loc[(df['infection_status'] == status)]['t1_latency_to_exit_carrier'])
 
             neg, pos = slct('Negative'), slct('Positive')
-
             stat, p = mannwhitneyu(neg, pos)
             stat_values = statVal(stat, p)
             result = alphaTest(p)
@@ -84,8 +105,16 @@ def MannWhitney_U(csv_file, test, drop_non_dlc=True, only_sig=False):
 
         latency_mw(csv_file)
 
+    """
+    << Time Spent in Regions of Interest >>
+    Video pixel coordinates for the DeepLabCut-generated labels were used to
+    he average time a cat spent within 80 pixels (~0.80 meters) of the walls
+    (as opposed to being in the center) of the experimental room. 
+    """
+
     elif test == 'roi':
-        def roi_calc_mw(csv_file, drop_non_dlc=False):
+
+        def roi_mw(csv_file, drop_non_dlc=False):
 
             df = pd.read_csv(csv_file)
 
@@ -96,7 +125,6 @@ def MannWhitney_U(csv_file, test, drop_non_dlc=True, only_sig=False):
                 return df.loc[(df['trial'] == i) & (df['infection_status'] == s) & (df['ROI_name'] == j)]
 
             for j in ['walls', 'middle']:
-
                 print(f'\n{j}')
 
                 for i in trls:
@@ -107,4 +135,28 @@ def MannWhitney_U(csv_file, test, drop_non_dlc=True, only_sig=False):
                         stat, p = mannwhitneyu(neg[voi], pos[voi])
                         print(f'{voi} ==> {statVal(stat, p)}, {alphaTest(p)}')
 
-        roi_calc_mw(csv_file)
+        roi_mw(csv_file)
+
+    """
+    << Sniffing Treatment Vial >>
+    a single behavior “sniff treatment vial” was used in
+    the “Sniffing Treatment Vial” test. 
+    """
+
+    elif test == 'sniffing_vial':
+
+        def sniff_mw(csv_file):
+
+            df = pd.read_csv(csv_file)
+
+            behaviors = ['t3_sniffsaline', 't5_sniffurine', 't7_sniffsaline', 't9_sniffurine']
+
+            def slct(s, i):
+                return list(df.loc[(df['infection_status'] == s)][i])
+
+            for i in behaviors:
+                neg. pos = slct('Negative', i), slct('Positive', i)
+                stat, p = mannwhitneyu(neg, pos)
+                stat_values = statVal(stat, p)
+                result = alphaTest(p)
+                print(f'{i} ==> {stat_values}, {result}')
