70d4f597f7a84108a933c06ad216add21956a76b
Alyetama <56323389+Alyetama@users.noreply.github.com>
2020-09-19 19:41:08 -0500

    :art: Improve code quality & add new func to MW..()

--- toxopy/MannWhitney_U.py
+++ toxopy/MannWhitney_U.py
@@ -6,33 +6,29 @@ Licensed under the terms of the MIT license
 
 from scipy.stats import mannwhitneyu
 import pandas as pd
-from toxopy import trials, nadlc
+from toxopy import trials, nadlc, roi_behaviors, sniff_instances
+from itertools import combinations
 
 
-def MannWhitney_U(csv_file, test, drop_non_dlc=True, only_sig=False):
+class MannWhitney_U:
 
-    df = pd.read_csv(csv_file)
-
-    excluded_cats = nadlc()
-
-    trls = trials()
-
-    if drop_non_dlc is True:
-        for c in excluded_cats:
-            df.drop(df[df.cat == c].index, inplace=True)
-
-    alpha = 0.05
-
-    def statVal(stat, p):
-        return 'Statistics=%.3f, p=%.3f' % (stat, p)
+    def read(csv_file, drop_non_dlc):
+        df = pd.read_csv(csv_file)
+        excluded_cats, trls = nadlc(), trials()
+        if drop_non_dlc is True:
+            for c in excluded_cats:
+                df.drop(df[df.cat == c].index, inplace=True)
+        return df
 
     def alphaTest(p):
+        alpha = 0.05
         if p > alpha:
             result = 'fail to reject H0'
         else:
             result = 'reject H0'
         return result
 
+<<<<<<< Updated upstream
     if test == 'time_budget':
 
         def time_budget_mw(csv_file, only_sig):
@@ -81,22 +77,34 @@ def MannWhitney_U(csv_file, test, drop_non_dlc=True, only_sig=False):
         time_budget_mw(csv_file, only_sig)
 
     elif test == 'latency':
+=======
+    def statVal(stat, p):
+        res = 'Statistics=%.3f, p=%.3f' % (stat, p)
+        return res
 
-        def latency_mw(csv_file):
-            """
-            << The Latency Test >>
-            The latency of the cat to exit the carrier, the latency test, was defined
-            as the time from when the owner opened the carrier to the time when the
-            cat’s entire body was outside of the carrier. Scores on a single behavior
-            “latency to exit the carrier” were used to compute results for
-            the latency test. 
-            """
 
-            def slct(status):
-                return list(df.loc[(df['infection_status'] == status)]['t1_latency_to_exit_carrier'])
+    def time_budget_mw(csv_file, only_sig=False):
+        """
+        << TIME BUDGET >>
+        The time budget of a cat for a trial was determined by summing the times it spent on the individual behaviors assigned to each
+        behavioral category (Affiliative, Calm, Exploration/Locomotion,
+        and Fear). 
+        """
+        df = MannWhitney_U.read(csv_file, drop_non_dlc)
+>>>>>>> Stashed changes
 
-            neg, pos = slct('Negative'), slct('Positive')
+        behaviors = ['Exploration/locomotion',
+                     'Fear', 'Calm', 'Affiliative']
+
+        def calc_mw(t, b):
+            def slct(s, t, b):
+                return list(df.loc[(df['infection_status'] == s)
+                                   & (df['trial'] == t) &
+                                   ((df['Behavior'] == b))]['value'])
+
+            neg, pos = slct('Negative', t, b), slct('Positive', t, b)
             stat, p = mannwhitneyu(neg, pos)
+<<<<<<< Updated upstream
             stat_values = statVal(stat, p)
             result = alphaTest(p)
 
@@ -105,56 +113,153 @@ def MannWhitney_U(csv_file, test, drop_non_dlc=True, only_sig=False):
         latency_mw(csv_file)
 
     elif test == 'roi':
+=======
+            stat_values = MannWhitney_U.statVal(stat, p)
 
-        def roi_mw(csv_file, drop_non_dlc=False):
-            """
-            << Time Spent in Regions of Interest >>
-            Video pixel coordinates for the DeepLabCut-generated labels were used to
-            he average time a cat spent within 80 pixels (~0.80 meters) of the walls
-            (as opposed to being in the center) of the experimental room. 
-            """
+            if only_sig is False:
+                result = MannWhitney_U.alphaTest(p)
+                return stat_values, result
+            else:
+                if p < alpha:
+                    result = 'reject H0'
+                    return stat_values, result
 
-            df = pd.read_csv(csv_file)
+        for t in trls:
+            print(f'{"-" * 60}\n{t}')
+            for b in behaviors:
+                mw_res = calc_mw(t, b)
+                res = f'{b} ==> {mw_res}'
+                if mw_res is not None:
+                    print(res)
+>>>>>>> Stashed changes
 
-            vois = ["cumulative_time_in_roi_sec",
-                    "avg_time_in_roi_sec", "avg_vel_in_roi"]
 
-            def slct(i, s, j):
-                return df.loc[(df['trial'] == i) & (df['infection_status'] == s) & (df['ROI_name'] == j)]
+    def latency_mw(csv_file, drop_non_dlc):
+        """
+        << The Latency Test >>
+        Scores on a single behavior “latency to exit the carrier” were
+        used to compute results for the latency test. 
+        """
+        df = MannWhitney_U.read(csv_file, drop_non_dlc)
 
-            for j in ['walls', 'middle']:
-                print(f'\n{j}')
+        def slct(status):
+            return list(df.loc[(df['infection_status'] == status)]['t1_latency_to_exit_carrier'])
 
-                for i in trls:
-                    pos, neg = slct(i, 'Positive', j), slct(i, 'Negative', j)
-                    print(f'\n{i}')
+        neg, pos = slct('Negative'), slct('Positive')
+        stat, p = mannwhitneyu(neg, pos)
+        stat_values = MannWhitney_U.statVal(stat, p)
+        result = MannWhitney_U.alphaTest(p)
 
-                    for voi in vois:
-                        stat, p = mannwhitneyu(neg[voi], pos[voi])
-                        print(f'{voi} ==> {statVal(stat, p)}, {alphaTest(p)}')
+        print(f'Latency to exit the carrier ==> {stat_values}, {result}')
 
-        roi_mw(csv_file)
 
+    def roi_mw(csv_file):
+        """
+        << Time Spent in Regions of Interest (ROIs) >>
+        Video pixel coordinates for the DeepLabCut-generated labels were
+        used to calculate the average time a cat spent near the walls (as opposed to being in the center) in the experimental room. 
+        """
+        df = MannWhitney_U.read(csv_file)
+
+        vois = roi_behaviors()
+
+<<<<<<< Updated upstream
     elif test == 'sniffing_vial':
+=======
+        def slct(i, s, j):
+            return df.loc[(df['trial'] == i) & (df['infection_status'] == s) & (df['ROI_name'] == j)]
 
-        def sniff_mw(csv_file):
-            """
-            << Sniffing Treatment Vial >>
-            a single behavior “sniff treatment vial” was used in
-            the “Sniffing Treatment Vial” test. 
-            """
+        for j in ['walls', 'middle']:
+            print(f'\n{j}')
+            for i in trls:
+                pos, neg = slct(i, 'Positive', j), slct(i, 'Negative', j)
+                print(f'\n{i}')
+                for voi in vois:
+                    stat, p = mannwhitneyu(neg[voi], pos[voi])
+                    print(f'{voi} ==> {MannWhitney_U.statVal(stat, p)}, {MannWhitney_U.alphaTest(p)}')
+>>>>>>> Stashed changes
 
-            df = pd.read_csv(csv_file)
 
-            behaviors = ['t3_sniffsaline', 't5_sniffurine',
-                         't7_sniffsaline', 't9_sniffurine']
+    def sniff_mw(csv_file, drop_non_dlc):
+        """
+        << Sniffing Treatment Vial >>
+        a single behavior “sniff treatment vial” was used in the “Sniffing Treatment Vial” test. 
+        """
+        df = MannWhitney_U.read(csv_file, drop_non_dlc)
 
-            def slct(s, i):
-                return list(df.loc[(df['infection_status'] == s)][i])
+        sniff_inst = sniff_instances()
 
-            for i in behaviors:
-                neg. pos = slct('Negative', i), slct('Positive', i)
-                stat, p = mannwhitneyu(neg, pos)
-                stat_values = statVal(stat, p)
-                result = alphaTest(p)
-                print(f'{i} ==> {stat_values}, {result}')
+        def slct(s, i):
+            return list(df.loc[(df['infection_status'] == s)][i])
+
+        for i in sniff_inst:
+            neg, pos = slct('Negative', i), slct('Positive', i)
+            stat, p = mannwhitneyu(neg, pos)
+            stat_values = MannWhitney_U.statVal(stat, p)
+            result = MannWhitney_U.alphaTest(p)
+            print(f'{i} ==> {stat_values}, {result}')
+
+
+    def roi_diff_Btrials_Wgroup(csv_file, comparison, trial_type=None, export_csv=False):
+        """
+        << Time Spent in ROIs – Within-group >>
+        Similar to 'roi'. Except it compares time spent in ROI *within* group between trials.
+        """
+        df = MannWhitney_U.read(csv_file)
+
+        def slct(tr):
+            if comparison == 'all':
+                return df.loc[(df['ROI_name'] == 'walls')
+                          & (df['infection_status'] == k) &
+                          (df['trial'] == tr)][b]
+            elif comparison == 'split':
+                return df.loc[(df['ROI_name'] == 'walls') & (df['infection_status'] == k) & df.trial.isin(tr)][b]
+
+        def res(s):
+            if comparison == 'all':
+                pt_comp = f'{combs[i][0]} vs {combs[i][1]}'
+            elif comparison == 'split':
+                pt_comp = '1st-half vs 2nd-half'
+            return f'{pt_comp}{s}{stat}{s}{round(p, 4)}{s}{result}'
+
+        def ttype(rng):
+            return list(combinations(rng, 2))
+
+        trls, vois = trials(), roi_behaviors()
+
+        if comparison == 'all' and trial_type is None:
+            raise ValueError('Missing argument: "trial_type"!')
+        if comparison == 'split' and trial_type is not None:
+            raise ValueError('"split" cannot take a "trial_type" argument')
+
+        if trial_type == 'treatment':
+            ombs, r = ttype(trls[::2][1:]), range(0, 6)
+        elif trial_type == 'CA':
+            combs, r = ttype(trls[1::2]), range(0, 10)
+        else:
+            r = range(0, 1)
+
+        if export_csv is True:
+            f = open('results.csv', 'w')
+            print('status,comparison,stat,p,interpretation', file=f)
+
+        for k in ['Negative', 'Positive']:
+            if export_csv is not True:
+                print(f'{"-" * 65}\n<< {k} >>')
+            for b in vois:
+                if export_csv is not True:
+                    print(f'\n#{b}')
+                for i in r:
+                    if comparison == 'all':
+                        c1, c2 = slct(combs[i][0]), slct(combs[i][1])
+                    elif comparison == 'split':
+                        c1, c2 = slct(trls[0:5]), slct(trls[5:10])
+                    stat, p = mannwhitneyu(c1, c2)
+                    result = MannWhitney_U.alphaTest(p)
+                    if export_csv is not True:
+                        print(res(' ==> '))
+                    else:
+                        print(f'{k},', res(','), file=f)
+
+        if export_csv is True:
+            f.close()
