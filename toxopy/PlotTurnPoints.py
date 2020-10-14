5e0aeceb24717f9b20153b2d38749a89c8b5f66d
fcatus <f.catus@pm.me>
2020-10-13 22:04:49 -0500

    :package: Add PlotTurnPoints() func

---
+++ toxopy/PlotTurnPoints.py
@@ -0,0 +1,120 @@
+"""
+Toxopy (https://github.com/bchaselab/Toxopy)
+© M. Alyetama, University of Nebraska at Omaha
+Licensed under the terms of the MIT license
+"""
+
+from rdp import rdp
+import pandas as pd
+import numpy as np
+from toxopy import fwarnings
+import matplotlib.pyplot as plt
+import seaborn as sns
+from glob import glob
+from pathlib import Path
+# %matplotlib inline
+
+
+def plotTurnpoints(csv_file, turnpoints_dir):
+
+    cats = sorted(list(pd.read_csv(csv_file).cat.unique()))
+    positions = range(221, 225)
+
+    DIR = sorted(glob(f'{turnpoints_dir}/*.csv'))
+
+    files = []
+    for i in sorted(DIR):
+        if Path(i).stem in cats:
+            files.append(i)
+
+    def PlotCat(file, cat, pos):
+
+        df = pd.read_csv(csv_file)
+        tp = pd.read_csv(file)
+
+        def slct(cat, coord):
+            return df.loc[(df['cat'] == cat)][coord].to_numpy()
+
+        df = df.loc[(df['cat'] == cat)]
+
+        x, y = slct(cat, 'time'), slct(cat, 'velocity_loess05')
+
+        def alt(firstispeak):
+            if firstispeak == 'True':
+                return ['bo', 'ro']
+            if firstispeak == 'False':
+                return ['ro', 'bo']
+            return None
+
+        firstispeak = str(tp['firstispeak'][0])
+        P = alt(firstispeak)
+
+        trajectory = pd.DataFrame([x, y])
+        trajectory = trajectory.to_numpy().reshape(len(x), 2)
+
+        idx = tp['tppos'].to_numpy()
+
+        sns.set(style='ticks', font_scale=1)
+        fig = plt.gcf()
+        fig.set_size_inches(20, 12)
+
+        ax = fig.add_subplot(pos)
+        ax.plot(x, y, 'k-', label='Trajectory')
+
+        for i, j, k in zip([0, 1], [P[0], P[1]], ['Maximum', 'Minimum']):
+            ax.plot(x[idx][i::2],
+                    y[idx][i::2],
+                    j,
+                    mfc='none',
+                    label=f'Local {k}')
+
+        times = [0, 300, 420, 600, 720, 900, 1020, 1200, 1320, 1500, 1620]
+
+        for i in times:
+            plt.axvline(x=i,
+                        linestyle='--',
+                        color='gray',
+                        label='Trial Boundary')
+
+        handles, labels = plt.gca().get_legend_handles_labels()
+        order = [3, 0, 1, 2]
+        labels_handles = {
+            label: handle
+            for ax in fig.axes
+            for handle, label in zip(*ax.get_legend_handles_labels())
+        }
+        ax.legend(
+            [handles[q] for q in order],
+            [labels[q] for q in order],
+            loc="upper right",
+            bbox_to_anchor=(1, 0.88),
+            bbox_transform=plt.gcf().transFigure,
+        )
+
+    fig, ax = plt.subplots(nrows=0,
+                           ncols=0,
+                           sharex=True,
+                           sharey=True,
+                           figsize=(6, 6))
+
+    font = {'family': 'DejaVu Sans', 'weight': 'regular', 'size': 18}
+
+    plt.rc('font', **font)
+
+    fig.text(0.5, 0.07, 'Time', ha='center')
+    fig.text(0.07,
+             0.5,
+             'Speed in Pixels per Second',
+             va='center',
+             rotation='vertical')
+
+    for file, cat, pos in zip(files, cats, positions):
+        PlotCat(file, cat, pos)
+
+    plt.show()
+
+
+plotTurnpoints(
+    "/Users/Felis.catus/Desktop/combined_4.csv",
+    '/Users/Felis.catus/Desktop/GitHub/bchaselab/DLC-Chaselab/Data/dATA_turnpoints/single_files'
+)
--- toxopy/dlcboxplot.py
+++ toxopy/dlcboxplot.py
@@ -19,82 +19,86 @@ def dlcboxplot(file,
                save=False,
                output_dir=False):
 
-    df = pd.read_csv(file)
-    df = df[df['var'] == variable]
+  """
+  file is typically 'dlc_all_avgs_updated.csv'
+  variable is either 'cat_ditance' or 'vel'
+  ylab is the y-axis label
+  colors is a list of two colors (e.g., ["#0062FF", "#DB62FF"])
+  output_dir to ave the plot in a specific dir when save is True
+  """
 
-    sns.set(style="ticks", font_scale=1)
+  df = pd.read_csv(file)
+  df = df[df['var'] == variable]
 
-    plt.figure(figsize=(13, 5), dpi=100)
+  sns.set(style='ticks', font_scale=1)
 
-    if colors is False:
-        my_pal = {"Negative": "#00FFFF", "Positive": "#E60E3C"}
-    else:
-        my_pal = {"Negative": colors[0], "Positive": colors[1]}
+  plt.figure(figsize=(13, 5), dpi=100)
 
-    ax = sns.boxplot(x='trial',
-                     y='value',
-                     data=df,
-                     hue='status',
-                     palette=my_pal)
+  if colors is False:
+      my_pal = {'Control': '#00FFFF', 'Infected': '#E60E3C'}
+  else:
+      my_pal = {'Control': colors[0], 'Infected': colors[1]}
 
-    if jitter is True:
+  ax = sns.boxplot(x='trial',
+                   y='value',
+                   data=df,
+                   hue='infection_status',
+                   palette=my_pal)
 
-        sns.stripplot(x='trial',
-                      y='value',
-                      data=df,
-                      color='black',
-                      size=3,
-                      jitter=1)
-    '''add vertical lines to seperate boxplots pairs (style)'''
-    for i in range(len(df['trial'].unique()) - 1):
-        if variable == 'vel':
-            plt.vlines(i + .5,
-                       10,
-                       45,
-                       linestyles='solid',
-                       colors='black',
-                       alpha=0.2)
-        elif variable == 'cat_distance':
-            plt.vlines(i + .5,
-                       0,
-                       1.3,
-                       linestyles='solid',
-                       colors='black',
-                       alpha=0.2)
+  if jitter is True:
+      sns.stripplot(x='trial',
+                    y='value',
+                    data=df,
+                    color='black',
+                    size=3,
+                    jitter=1)
 
-    if title is not False:
-        plt.title(title, fontsize=14)
-    else:
-        pass
+  for i in range(len(df['trial'].unique()) - 1):
+    def vLines(i, j):
+      '''add vertical lines to seperate boxplots pairs (style)'''
+      return plt.vlines(i + .5,
+                     i,
+                     j,
+                     linestyles='solid',
+                     colors='black',
+                     alpha=0.2)
+      if variable == 'vel':
+        vLines(10, 45)
+      elif variable == 'cat_distance':
+        vLines(0, 1.3)
 
-    ax.set(xlabel='Trial', ylabel=ylab)
+  if title is not False:
+      plt.title(title, fontsize=14)
+  else:
+      pass
 
-    plt.legend(title='Infection Status')
-    '''add significance bars and asterisks between boxes'''
-    # [first pair, second pair], ..., [|, –], ...
-    if variable == 'vel':
-        l = [[7.75, 5.75], [8.25, 6.25], [26, 28], [31, 33]]
-    elif variable == 'cat_distance':
-        l = [[7.75, 5.75], [8.25, 6.25], [0.85, 0.9], [0.95, 1]]
+  ax.set(xlabel='Trial', ylabel=ylab)
 
-    for x1, x2, y1, y2 in zip(l[0], l[1], l[2], l[3]):
-        sig = plt.plot([x1, x1, x2, x2], [y1, y2, y2, y1],
-                       linewidth=1,
-                       color='k')
-        plt.text((x1 + x2) * .5, y2 + 0, "*", ha='center', va='bottom')
+  plt.legend(title='Infection Status')
+  '''add significance bars and asterisks between boxes'''
+  # [first pair, second pair], ..., [|, –], ...
+  if variable == 'vel':
+      l = [[7.75, 5.75], [8.25, 6.25], [26, 28], [31, 33]]
+  elif variable == 'cat_distance':
+      l = [[7.75, 5.75], [8.25, 6.25], [0.85, 0.9], [0.95, 1]]
 
-    plt.show()
+  for x1, x2, y1, y2 in zip(l[0], l[1], l[2], l[3]):
+      sig = plt.plot([x1, x1, x2, x2], [y1, y2, y2, y1],
+                     linewidth=1,
+                     color='k')
+      plt.text((x1 + x2) * .5, y2 + 0, "*", ha='center', va='bottom')
 
-    fig = ax.get_figure()
+  plt.show()
 
-    if save is True:
-        if output_dir is not False:
-            fig.savefig(f'{output_dir}/{variable}.png',
-                        bbox_inches="tight",
-                        dpi=100,
-                        pad_inches=0.1)
-        else:
-            fig.savefig(f'{variable}.png',
-                        bbox_inches="tight",
-                        dpi=100,
-                        pad_inches=0.1)
+  fig = ax.get_figure()
+
+  if save is True:
+    def sav(myString):
+      return fig.savefig(myString,
+                      bbox_inches='tight',
+                      dpi=100,
+                      pad_inches=0.1)
+      if output_dir is not False:
+        sav(f'{output_dir}/{variable}.png')
+      else:
+        sav(f'{variable}.png')
